6cdc9f6a2a2c58fc2e59236a52b8c80d882432c6
Alex <bigpewm@gmail.com>
2020-07-06 05:18:59 +0300

    Ver 0.2

---
+++ pending-comments/template.php
@@ -0,0 +1,3 @@
+<script><?php print(file_get_contents(__DIR__ . '/scripts.js'))?></script>
+<a href="#comment-add" class="hide-if-no-js taxonomy-add-new" onclick="addNewComment(this)">+ Добавить комментарий</a>
+<input type='hidden' id='pendingCommentsComplete' name='pendingCommentsComplete'>

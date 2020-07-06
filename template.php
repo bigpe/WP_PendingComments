b53994cefb75a3049aeaf61d281a236b901f53cf
bigpe <52139992+bigpe@users.noreply.github.com>
2020-07-06 05:23:27 +0300

    Delete template.php

--- template.php
+++
@@ -1,3 +0,0 @@
-<script><?php print(file_get_contents(__DIR__ . '/scripts.js'))?></script>
-<a href="#comment-add" class="hide-if-no-js taxonomy-add-new" onclick="addNewComment(this)">+ Добавить комментарий</a>
-<input type='hidden' id='pendingCommentsComplete' name='pendingCommentsComplete'>
